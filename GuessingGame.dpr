0745bcb412e5c0f560a76d79fde70cca99ac1130
RhysJones1 <91537105+RhysJones1@users.noreply.github.com>
2022-01-12 13:32:18 +0000

    Add files via upload

---
+++ GuessingGame.dpr
@@ -0,0 +1,35 @@
+program GuessingGame;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  SysUtils,
+  Vcl.Dialogs,
+  Character,
+  Math;
+
+var
+  W, X, Y: Integer; // These will be used within the guessing game
+
+begin
+  try
+     // Use Repeat Until to create a guessing game
+  W := RandomRange(0,10); // Number we try to guess
+  X := 0; // Our guess
+  Repeat
+    Write('Guess the Number : ');
+    Readln(X);
+    if(X > W) then
+      Writeln('To Big');
+    if(X < W) then
+      Writeln('To Small');
+  Until W = X;
+  Writeln('You Guessed It!');
+  Readln;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
